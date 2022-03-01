a9edc42112d464f84fbee0dc16aeb53f0b0fd9ba
Varun Narravula <varun@snare.dev>
2022-03-01 12:10:56 -0800

    Small improvements
    - Add lockfile to watcher
    - Add result directories to .gitignore

--- Notes/watch.py
+++ Notes/watch.py
@@ -2,6 +2,7 @@
 Watch notes and rebuild PDFs if they are modified or created
 """
 
+import os
 import sys
 import subprocess
 import time
@@ -11,7 +12,7 @@ from watchdog.events import FileClosedEvent, PatternMatchingEventHandler
 
 class Watch:
     def __init__(self, directory: str) -> None:
-        self.directory = directory 
+        self.directory = directory
         self.observer = Observer()
 
     def run(self):
@@ -21,7 +22,7 @@ class Watch:
         try:
             while True:
                 time.sleep(5)
-        except:
+        except Exception:
             print("Stopping observer")
         finally:
             self.observer.stop()
@@ -29,17 +30,65 @@ class Watch:
 
 
 class Handler(PatternMatchingEventHandler):
-    def __init__(self, patterns=None, ignore_patterns=None, ignore_directories=True, case_sensitive=True):
-        super().__init__(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    def __init__(
+        self,
+        patterns=None,
+        ignore_patterns=None,
+        ignore_directories=True,
+        case_sensitive=True,
+    ):
+        super().__init__(
+            patterns,
+            ignore_patterns,
+            ignore_directories,
+            case_sensitive,
+        )
 
     @staticmethod
     def on_closed(event: FileClosedEvent) -> None:
         if event.is_directory or event.is_synthetic:
             return
         destination = event.src_path.replace(".md", ".pdf")
-        subprocess.run(["pandoc", "-f", "markdown", event.src_path, "-o", destination]) 
-        
+        subprocess.run(
+            [
+                "pandoc",
+                "-f",
+                "markdown",
+                event.src_path,
+                "-o",
+                destination,
+            ]
+        )
+
+
+class LockFile:
+    def __init__(self, file_name: str):
+        self.is_locked = False
+        self.lockfile = file_name
+
+    def create(self):
+        try:
+            self.handle = os.open(
+                self.lockfile,
+                os.O_CREAT | os.O_EXCL | os.O_RDWR,
+            )
+            self.is_locked = True
+        except Exception:
+            print("Could not lock process. Is there another watcher running?")
+            exit(1)
+
+    def release(self):
+        os.close(self.handle)
+        os.unlink(self.lockfile)
+
+    def __enter__(self):
+        return self.handle if self.is_locked else self.create()
+
+    def __exit__(self):
+        pass
+
 
 if __name__ == "__main__":
-    watch = Watch(sys.argv[1] if len(sys.argv) > 1 else "./")
-    watch.run()
+    with LockFile("/tmp/.cs188-watch.lock"):
+        watch = Watch(sys.argv[1] if len(sys.argv) > 1 else "./")
+        watch.run()
